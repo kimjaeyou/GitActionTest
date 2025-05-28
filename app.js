14e4b9ed2b16ced49e52faf3cbfde425779e6352
kimjaeyou <unknown88455144@gmail.com>
2025-05-28 15:38:58 +0900

    1538

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD!!!!!!!!!!!!From GitHub Actions 25/05/28-15:34');
+  res.send('Hello CI/CD!!!!!!!!!!!!From GitHub Actions 25/05/28-15:38');
 });
 
 const PORT = process.env.PORT || 3000;
